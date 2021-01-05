5969ecf5cbb1d8655d2ab0fcfed8f3d88ee5a85b
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-01-05 13:21:36 -0300

    Initial Commit

---
+++ Storage.Tests/Program.cs
@@ -0,0 +1,33 @@
+﻿using Isaac.FileStorage;
+using System;
+
+namespace Storage.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var w = new Core("C:\\sblevers\\");
+
+            test t = new()
+            {
+                CPF = "0",
+                Nome = "Joaquim"
+            };
+
+            w.Insert("321", t);
+
+            var specifickey = w.Get<test>("321");
+
+            var keys = w.GetAllKeys().
+
+            keys = keys;
+        }
+    }
+
+    public class test
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}
---
+++ Storage/Core.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Isaac.FileStorage
+{
+    public class Core
+    {
+        public string DirectoryPath { get; }
+
+        public Core(string Path)
+        {
+            if(Path == null) throw new ArgumentNullException(nameof(Path));
+
+            var di = new DirectoryInfo(Path);
+
+            if (!di.Exists) di.Create();
+
+            DirectoryPath = di.FullName;
+        }
+
+        public void Insert<T>(string key, T obj) 
+        {
+            var serialized = JsonConvert.SerializeObject(obj);
+            var filename = getFileName(key);
+
+            File.WriteAllText(filename, serialized);
+        }
+        public T Get<T>(string key)
+        {
+            var file = getFileName(key);
+            var json = File.ReadAllText(file);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+        public IEnumerable<string> GetAllKeys()
+        {
+            return Directory.GetFiles(DirectoryPath, "*.jk")
+                            .Select(o => new FileInfo(o).Name
+                                                        .Replace(".jk", ""));
+        }
+
+        private string getFileName(string key)
+        {
+            return Path.Combine(DirectoryPath, $"{key}.jk");
+        }
+    }
+}
