2e6694724e241cff0e9325e52615e25d0d2f2764
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-01-06 17:44:17 -0300

    Update TestBlock.cs
    Inserting mistakes to future explosions

--- FileStorage.UnitTest/TestBlock.cs
+++ FileStorage.UnitTest/TestBlock.cs
@@ -1,21 +1,51 @@
 ﻿using Isaac.FileStorage;
 using System;
 using System.IO;
+using System.Threading;
 
 namespace FileStorage.UnitTest
 {
     public class TestBlock : IDisposable
     {
-        public Core db { get; }
+        static object lockObj;
+        static TestBlock()
+        {
+            lockObj = new object();
+        }
 
+        public Core db { get; }
+        
         public TestBlock()
         {
             db = new Core("unitTestTests");
+            nukeDB();
         }
 
         public void Dispose()
         {
-            Directory.Delete(db.DirectoryPath, true);
+            nukeDB();
+        }
+
+        void nukeDB()
+        {
+            // I know this is bad
+            // I know I should not have done this
+            // But I am desperate at this point
+            // nothing else worked
+            lock (lockObj)
+            {
+                for (int retries = 0; retries < 5; retries++)
+                {
+                    try
+                    {
+                        foreach (var f in Directory.GetFiles(db.DirectoryPath))
+                        {
+                            File.Delete(f);
+                        }
+                    }
+                    catch { Thread.Sleep(50); }
+                }
+            }
         }
     }
 }
