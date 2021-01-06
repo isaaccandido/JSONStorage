248672d6f27c05818c87320e230144e355b7a796
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-01-06 17:46:24 -0300

    Update TestBlock.cs

--- FileStorage.UnitTest/TestBlock.cs
+++ FileStorage.UnitTest/TestBlock.cs
@@ -7,45 +7,17 @@ namespace FileStorage.UnitTest
 {
     public class TestBlock : IDisposable
     {
-        static object lockObj;
-        static TestBlock()
-        {
-            lockObj = new object();
-        }
-
         public Core db { get; }
         
         public TestBlock()
         {
             db = new Core("unitTestTests");
-            nukeDB();
         }
 
         public void Dispose()
         {
-            nukeDB();
-        }
+            Directory.Delete(db.DirectoryPath, true);
 
-        void nukeDB()
-        {
-            // I know this is bad
-            // I know I should not have done this
-            // But I am desperate at this point
-            // nothing else worked
-            lock (lockObj)
-            {
-                for (int retries = 0; retries < 5; retries++)
-                {
-                    try
-                    {
-                        foreach (var f in Directory.GetFiles(db.DirectoryPath))
-                        {
-                            File.Delete(f);
-                        }
-                    }
-                    catch { Thread.Sleep(50); }
-                }
-            }
         }
     }
 }
