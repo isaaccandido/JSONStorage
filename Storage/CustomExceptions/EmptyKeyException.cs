c37959ebdec745db36c0aa6742cccea5a3ca9324
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-01-15 12:04:47 -0300

    added custom exceptions

---
+++ Storage/CustomExceptions/EmptyKeyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Isaac.FileStorage
+{
+    public class EmptyKeyException : Exception
+    {
+        public override string Message { get; }
+        public EmptyKeyException() : base() => Message = "Key cannot be empty.";
+        public EmptyKeyException(string Message) => this.Message = Message;
+    }
+}
