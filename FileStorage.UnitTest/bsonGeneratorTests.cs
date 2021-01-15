a114d15ba51b33fc26b58aedff030eb0a22657af
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-01-15 20:00:18 -0300

    Create bsonGeneratorTests.cs
    Added tests for null support

---
+++ FileStorage.UnitTest/bsonGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FileStorage.UnitTest
+{
+    public class bsonGeneratorTests
+    {
+        [Fact]
+        public static void bsonGenerator_NullValue()
+        {
+            using var block = new TestBlock();
+            block.db.Insert<Transaction>("1234", null);
+            var nullClass = block.db.Get<Transaction>("1234");
+
+            Assert.Null(nullClass);
+        }
+    }
+}
